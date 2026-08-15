e0ed84b231bfef1bb61c24531b6c80977f0dba3d
Skixkk <skixkk7@gmail.com>
2026-08-15 13:59:44 +0800

    feat(favicon): :lipstick: Add icon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,15 +1,28 @@
+/*
+ * @Author: Skixkk skixkk7@gmail.com
+ * @Date: 2026-08-14 22:45:25
+ * @LastEditors: Skixkk skixkk7@gmail.com
+ * @LastEditTime: 2026-08-15 13:55:50
+ * @FilePath: \merchant-admin\docs\.vitepress\config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   // app level config options
-  lang: 'en-US',
-  title: "merchant-admin",
-  description: "saas admin & guide",
   
   // 👇 新增这一行，默认构建在主站的 `/` ；下面设置为 VitePress 打包资源路径 `/mrechant-admin` 下
-  // 与仓库名完全一致，前后斜杠不可省略
+  // 与仓库名完全一致，形成主站  ，前后斜杠不可省略
   base: '/merchant-admin/',
+  lang: 'en-US',
+
+  title: "merchant-admin",
+  description: "saas admin & guide",
+  head: [
+    // 浏览器tab图标 favicon
+    ['link', { rel: 'icon', type:'image/x-icon', href: '/merchant-admin/favicon.ico' }]
+  ],
 
   themeConfig: {
     logo: {
